a3b9114472065e9b90c40431c78bba4a947a411d
MauricioSts <Mauricio.santos.110@ufrn.edu.br>
2025-04-04 14:42:41 -0300

    introducao a banco de dados

---
+++ android/app/src/main/kotlin/com/example/app_banco_de_dados/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_banco_de_dados
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:app_banco_de_dados/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: Home()));
+}
